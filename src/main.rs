c54f9c30f0878640f6f2ee7eea02475c2089e6e6
jdavasligil <22987230+jdavasligil@users.noreply.github.com>
2023-10-09 19:29:37 -0700

    Setup basic commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+// Copyright 2023, Jaedin Davasligil, All rights reserved.
+
+use clap::{Parser, Subcommand};
+
+/// Program to track developer time by clocking in and out
+#[derive(Parser, Debug)]
+struct Args {
+    /// The command to be called
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    In,
+    Out,
+}
+
+fn clock_in() {
+    println!("Clock in!")
+}
+
+fn clock_out() {
+    println!("Clock out!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Commands::In => clock_in(),
+        Commands::Out => clock_out(),
+    }
 }
